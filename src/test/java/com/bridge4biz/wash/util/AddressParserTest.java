9a86ecd19154961f6e1ea6f75b9e5fba5f13232d
ChaYongbin <theodore.cha@cleanbasket.co.kr>
2016-06-11 06:26:37 +0900

    remove test

--- src/test/java/com/bridge4biz/wash/util/AddressParserTest.java
+++ src/test/java/com/bridge4biz/wash/util/AddressParserTest.java
@@ -8,23 +8,23 @@ import com.bridge4biz.wash.service.District;
 
 public class AddressParserTest {
 
-	@Test
-	public final void testEnglishAddress() {
-		String address = "Hyochang-dong, Yongsan-gu, Seoul 110-22 105호";
-		
-		District objectDistrict = new District("Seoul", "Yongsan-gu", "Hyochang-dong");
-		District district = AddressParser.makeDistrict(address);
-		
-		assertEquals(objectDistrict.getCity(), district.getCity());
-		assertEquals(objectDistrict.getDistrict(), district.getDistrict());
-		assertEquals(objectDistrict.getDong(), district.getDong());
-		
-		String address2 = "Hyochang-dong, Yongsan-gu,";
-		
-		District objectDistrict2 = new District("Seoul", "Yongsan-gu", "Hyochang-dong");
-		District district2 = AddressParser.makeDistrict(address2);
-		
-		assertEquals(objectDistrict2.getDong(), district2.getDong());
-		assertEquals(objectDistrict2.getDistrict(), district2.getDistrict());
-	}
+//	@Test
+//	public final void testEnglishAddress() {
+//		String address = "Hyochang-dong, Yongsan-gu, Seoul 110-22 105호";
+//
+//		District objectDistrict = new District("Seoul", "Yongsan-gu", "Hyochang-dong");
+//		District district = AddressParser.makeDistrict(address);
+//
+//		assertEquals(objectDistrict.getCity(), district.getCity());
+//		assertEquals(objectDistrict.getDistrict(), district.getDistrict());
+//		assertEquals(objectDistrict.getDong(), district.getDong());
+//
+//		String address2 = "Hyochang-dong, Yongsan-gu,";
+//
+//		District objectDistrict2 = new District("Seoul", "Yongsan-gu", "Hyochang-dong");
+//		District district2 = AddressParser.makeDistrict(address2);
+//
+//		assertEquals(objectDistrict2.getDong(), district2.getDong());
+//		assertEquals(objectDistrict2.getDistrict(), district2.getDistrict());
+//	}
 }
