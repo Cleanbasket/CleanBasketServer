5b992af4f319565a612243925457656862ccb6b8
ChaYongbin <theodore.cha@cleanbasket.co.kr>
2016-08-19 19:03:24 +0900

    check response

--- src/main/java/com/bridge4biz/wash/DelivererController.java
+++ src/main/java/com/bridge4biz/wash/DelivererController.java
@@ -1,6 +1,8 @@
 package com.bridge4biz.wash;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -348,6 +350,27 @@ public class DelivererController {
 				os.write(jsonObject.toJSONString().getBytes());
 				os.flush();
 				os.close();
+
+				String response;
+
+				int responseCode = con.getResponseCode();
+
+				if(responseCode == HttpURLConnection.HTTP_OK) {
+
+					InputStream is = null;
+					ByteArrayOutputStream baos = null;
+					is = con.getInputStream();
+					baos = new ByteArrayOutputStream();
+					byte[] byteBuffer = new byte[1024];
+					byte[] byteData = null;
+					int nLength = 0;
+					while((nLength = is.read(byteBuffer, 0, byteBuffer.length)) != -1) {
+						baos.write(byteBuffer, 0, nLength);
+					}
+					byteData = baos.toByteArray();
+
+				}
+
 
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
