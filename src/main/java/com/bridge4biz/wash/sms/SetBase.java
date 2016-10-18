462e214ea7418bb23dbff4101f050b86fe386bc6
ChaYongbin <theodore.cha@cleanbasket.co.kr>
2016-10-18 12:07:30 +0900

    change cool sms api url

--- src/main/java/com/bridge4biz/wash/sms/SetBase.java
+++ src/main/java/com/bridge4biz/wash/sms/SetBase.java
@@ -4,7 +4,7 @@ package com.bridge4biz.wash.sms;
 public class SetBase {
 	private String api_key = "NCS54CAE83317A7F";
 	private String api_secret = "C224DA7674B9848F475E9683B87A7632"; 
-	private String base_url="https://api.coolsms.co.kr/1/";
+	private String base_url="https://api.coolsms.co.kr/sms/2/";
 	
 	public String getApiKey() {
 		return api_key;
